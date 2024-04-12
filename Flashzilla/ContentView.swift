e516c5c7d11e5eaa493c1612e34c85bcdc3db6f3
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-12 21:50:22 +0200

    feat: Ending the app with allowsHitTesting()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isActive = true
 
     var body: some View {
         ZStack {
@@ -16,6 +20,13 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -26,6 +37,20 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
